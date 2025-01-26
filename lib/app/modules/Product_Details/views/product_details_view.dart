9542edef98599f045d5df4a3cb195fb9f9cf5aaa
Jan Mahbub Milon <jan.milon.56@gmail.com>
2025-01-26 13:51:08 +0600

    Enhance Product Details View with improved UI elements and navigation

--- lib/app/modules/Product_Details/views/product_details_view.dart
+++ lib/app/modules/Product_Details/views/product_details_view.dart
@@ -1,3 +1,4 @@
+import 'package:chicken/app/data/AppColors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,9 +10,22 @@ class ProductDetailsView extends GetView<ProductDetailsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffE9E8E8),
       appBar: AppBar(
-        title: const Text('ProductDetailsView'),
-        centerTitle: true,
+        backgroundColor: primaryColor,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: white,
+          ),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        title: Text(
+          'Details',
+          style: TextStyle(color: white),
+        ),
       ),
       body: const Center(
         child: Text(
